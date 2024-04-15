0e0066a62351bcd30ee8801040600e7bba5dd485
anshjain321 <136158147+anshjain321@users.noreply.github.com>
2024-04-15 16:24:32 +0530

    project completed

--- Nasa exoplanet query front end/src/App.jsx
+++ Nasa exoplanet query front end/src/App.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import QueryPanel from './component/Query_panel';
+import ResultsPanel from './component/ResultPanel';
 function App() {
-  const [data, setdata] = useState(null);
+  const [data, setdata] = useState([]);
   const [isloading, setisloading] = useState(null);
   const [error, seterror] = useState(null);
 
-  useEffect(() => {
+  const [filteredData, setfilteredData] = useState([]);
+  const [resultvisible, setresultvisible] = useState(false);
+  useEffect(() => { 
     const fetchData = async ()=>{
       try{
-    const response  = await fetch('/api/data');
+    const response  = await fetch('http://localhost:3002/api/data');
     if(!response.ok){
       throw new Error('failed to fetch data');
     }  
     const jsonData = await response.json();
     setdata(jsonData);
+    setfilteredData(jsonData);
   }
   catch(error) {
    seterror(error.message);
@@ -36,11 +37,33 @@ function App() {
     return <div>Error: {error}</div>;
   }
 
+ 
+  const handleSearch = (query) => {
+    const filtered = data.filter((entry) => {
+      const nameMatch = entry.pl_name && entry.pl_name.toLowerCase().includes(query.pl_name.toLowerCase());
+      const hostMatch = entry.hostname && entry.hostname.toLowerCase().includes(query.hostname.toLowerCase());
+      const methodMatch = entry.discoverymethod && entry.discoverymethod.toLowerCase().includes(query.discoverymethod.toLowerCase());
+      const yearMatch = entry.disc_year && entry.disc_year.toString().includes(query.disc_year.toString());
+      const facilityMatch = entry.disc_facility && entry.disc_facility.toLowerCase().includes(query.disc_facility.toLowerCase());
+
+      return nameMatch && hostMatch && methodMatch && yearMatch && facilityMatch;
+    });
+
+    setfilteredData(filtered);
+    setresultvisible(true);
+  };
+
+  const handleClear = () => {
+    setfilteredData([]); 
+    setresultvisible(false); 
+  };
+
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <QueryPanel onSearch={handleSearch} exoplanetData={data} onClear={handleClear} />
+      {resultvisible && <ResultsPanel data={filteredData} />}
+    </div>
+  );
 }
 
 export default App
